1d9147ffdb8fddb2eb2b3ea94dbd7e9ecf1998bf
Mohamad Zabad <zabadmohamad@yahoo.com>
2017-02-21 17:45:24 +0000

    Checking if the table exists for this user

--- Login.php
+++ Login.php
@@ -12,13 +12,43 @@
 			<br><br> 
 			<input type="password" name="pass" placeholder="Password" maxlength="12" size="35" required></input>
 			<input type="Submit" name="submit" value="Sign In"></input>
-			<input onclick="window.location.href='http://localhost/PM3/register.php'" type="button" name="signup" value="Sign Up">
+			<input onclick="window.location.href='http://localhost/Password_Manager_v3.0/register.php'" type="button" name="signup" value="Sign Up">
 		</form>
 		<div id="two"> 
-			<h4 id="forgotpass"> Forgot your password? <a href="window.location.href='http://localhost/PM3/Recover.php'" id="clickhere"> Click Here!</a></h5>
+			<h4 id="forgotpass"> Forgot your password? <a href="window.location.href='http://localhost/Password_Manager_v3.0/Recover.php'" id="clickhere"> Click Here!</a></h5>
 			
 		</div>
 	</div>
 	
 	</body>
 </html>
+
+<?php
+	session_start();
+	unset($_SESSION["email"]);
+	unset($_SESSION["pass"]);
+	
+	$db = @mysqli_connect('localhost', 'root', '') or die('Fail message');
+	//  mysqli_select_db("book") or die("Fail message");    Choose table
+	
+	
+	if( isset($_POST["email"]) && isset($_POST["pass"]) )
+	{
+		
+
+		$email =mysqli_real_escape_string($db, $_POST['email']);
+		$pass = mysqli_real_escape_string($db, $_POST['pass']);
+	
+		if( $result = $db->query("SHOW TABLES LIKE '".$email."'") )
+		{
+			if($result->num_rows == 1)    // I wonder if I need this if statement 
+			{
+				echo "Table exists";
+			}
+		}
+		else
+		{
+			echo"No table";
+		}
+	}
+?> 
